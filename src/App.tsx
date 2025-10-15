11587c7b10e638291867020b79bd81c75513eabf
Venkat Avasarala <avenkash@gmail.com>
2025-10-15 11:48:05 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
     <div className="app">
       <Hero />
       <About />
-      <Services />
       <Gallery />
+      <Services />
       <Contact />
       <Footer />
     </div>
